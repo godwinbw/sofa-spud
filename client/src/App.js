f7fb1bbce683675da0d6c8c057fd9575d305f9f8
Hector Rangel <hector.rangeljr@yahoo.com>
2021-08-20 23:43:27 -0500

    aded auth.js and changed navbar to header

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import SearchBooks from './pages/SearchBooks';
 // import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import Navbar from './components/Header';
 
 function App() {
   return (
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,4 +9,4 @@ const Login = () => {
     )
 }
 
-export default LoginForm;
+export default Login;
