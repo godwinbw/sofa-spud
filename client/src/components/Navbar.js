a6cf178ba63ac65b6649cc34ad81f6289a063f80
Hector Rangel <hector.rangeljr@yahoo.com>
2021-08-23 16:19:13 -0500

    changed nav titles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,19 +15,19 @@ const AppNavbar = () => {
             <Navbar bg='dark' variant='dark' expand='lg'>
                 <Container fluid>
                     <Navbar.Brand as={Link} to='/'>
-                        Google Books Search
+                        Sofa Spud
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls='navbar' />
                     <Navbar.Collapse id='navbar'>
                         <Nav className='ml-auto'>
                             <Nav.Link as={Link} to='/'>
-                                Search For Books
+                                Search For Movies
                             </Nav.Link>
                             {/* if user is logged in show saved books and logout */}
                             {Auth.loggedIn() ? (
                                 <>
                                     <Nav.Link as={Link} to='/saved'>
-                                        See Your Books
+                                        See Your Movies
                                     </Nav.Link>
                                     <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                                 </>
