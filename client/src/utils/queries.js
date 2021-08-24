8f2cb1f511c55c97c90bab5b1812106b93f0b13e
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-08-24 17:22:57 -0500

    updated seeds

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -21,3 +21,29 @@ export const GET_USER = gql`
     }
   }
 `;
+
+export const SEARCH_FOR_TITLES = gql`
+  query searchForTitles($imdbId: String!) {
+    searchForTitlesTmdbApi(searchString: $imdbId) {
+      imdbId
+      title
+      titleType
+      plot
+      year
+      imageUrl
+    }
+  }
+`;
+
+export const SEARCH_FOR_SIMILAR_TITLES = gql`
+  query searchForSimilarTitles($imdbId: String!, $titleType: String!) {
+    searchForSimilarTitlesTmdbApi(imdbId: $imdbId, titleType: $titleType) {
+      imdbId
+      title
+      titleType
+      year
+      plot
+      imageUrl
+    }
+  }
+`;
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -11,31 +11,31 @@ db.once("open", async () => {
     password: "12345",
     watchList: [
       {
-        imdbId: "tt0944947",
+        imdbId: "1399",
         title: "Game of Thrones",
-        titleType: "tvSeries",
+        titleType: "tv",
         year: "2011",
         plot: "Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia.",
         imageUrl:
-          "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg",
+          "https://image.tmdb.org/t/p/w300/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg",
       },
       {
-        imdbId: "tt11823076",
+        imdbId: "100698",
         title: "Tiger King",
-        titleType: "tvMiniSeries",
+        titleType: "tv",
         year: "2020",
         plot: "A rivalry between big cat eccentrics takes a dark turn when Joe Exotic, a controversial animal park boss, is caught in a murder-for-hire plot.",
         imageUrl:
-          "https://m.media-amazon.com/images/M/MV5BYzI5MjQ2NzEtN2JmOC00MjE2LWI2NjItYTNjNTJjMjBkOWZkXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+          "https://image.tmdb.org/t/p/w300/pmjYMCnSwndlEpiFZhhOWSWmUsu.jpg",
       },
       {
-        imdbId: "tt1294706",
+        imdbId: "60763",
         title: "Parkour",
         titleType: "movie",
         year: "2009",
         plot: "Parkour is Marc Rensing's debut drama about a young man and his group of friends in an unnamed industrial town in Germany. The film is a captivating story of friendship, love, and jealousy and deals with Parkour, the physical discipline of training in which the traceur has to overcome obstacles within his or her path.",
         imageUrl:
-          "https://m.media-amazon.com/images/M/MV5BNzkwYzJkZjQtYjI1NC00NTQ3LWE3ZTEtOTI0ZDcxNGEzOWY1XkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_.jpg",
+          "https://image.tmdb.org/t/p/w300/4BGIPonTluukadGoBZZY9Z7CXJP.jpg",
       },
     ],
   });
@@ -47,22 +47,22 @@ db.once("open", async () => {
     password: "12345",
     watchList: [
       {
-        imdbId: "tt0080339",
+        imdbId: "813",
         title: "Airplane!",
         titleType: "movie",
         year: "1980",
         plot: "A man afraid to fly must ensure that a plane lands safely after the pilots become sick.",
         imageUrl:
-          "https://m.media-amazon.com/images/M/MV5BZjA3YjdhMWEtYjc2Ni00YzVlLWI0MTUtMGZmNTJjNmU0Yzk2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+          "https://image.tmdb.org/t/p/w300/7Q3efxd3AF1vQjlSxnlerSA7RzN.jpg",
       },
       {
-        imdbId: "tt0098800",
+        imdbId: "1892",
         title: "The Fresh Prince of Bel-Air",
-        titleType: "tvSeries",
+        titleType: "tv",
         year: "1990",
         plot: "A streetwise, poor young man from Philadelphia is sent by his mother to live with his aunt, uncle and cousins in their Bel-Air mansion.",
         imageUrl:
-          "https://m.media-amazon.com/images/M/MV5BOGUxOWQ4MzAtMmJjYS00M2U5LWEwZTAtYTc1YmZhNjg2NDRlXkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_.jpg",
+          "https://image.tmdb.org/t/p/w300/gYPE5vfStpVTf1ULE11B1Vwm12b.jpg",
       },
     ],
   });
