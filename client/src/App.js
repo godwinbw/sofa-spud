fcd383c857197ad4fb8ea7c8ccb1265e7578012e
Eric Webber <ewebber283@gmail.com>
2021-08-23 19:31:45 -0500

    switching search from component to page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import SearchBooks from './pages/SearchBooks';
 // import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import SearchTitles from './components/searchTitles';
+import SearchTitles from './components/SearchTitles';
 function App() {
   return (
     <Router>
