bca7a118cf9e57012bfe9f1d711e6b0adeaf2b79
Eric Webber <ewebber283@gmail.com>
2021-08-23 19:43:12 -0500

    set up api.js in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -27,21 +27,20 @@ export const getMe = (token) => {
     });
   };
   
-  // save book data for a logged in user
-  export const saveBook = (bookData, token) => {
+  export const saveMovie = (movieData, token) => {
     return fetch('/api/users', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify(bookData),
+      body: JSON.stringify(movieData),
     });
   };
   
   // remove saved book data for a logged in user
-  export const deleteBook = (bookId, token) => {
-    return fetch(`/api/users/books/${bookId}`, {
+  export const deleteMovie = (movieId, token) => {
+    return fetch(`/api/users/movies/${movieId}`, {
       method: 'DELETE',
       headers: {
         authorization: `Bearer ${token}`,
@@ -49,8 +48,6 @@ export const getMe = (token) => {
     });
   };
   
-  // make a search to google books api
-  // https://www.googleapis.com/books/v1/volumes?q=harry+potter
   export const searchGoogleBooks = (query) => {
     return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   };
