4ec41a0fa7549ac708dfd75151cbf7b97835cdf9
Hector Rangel <hector.rangeljr@yahoo.com>
2021-08-23 19:22:08 -0500

    apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient, inMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
@@ -34,9 +32,7 @@ function App() {
       <>
         <Navbar />
         <Switch>
-          {/* <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} /> */}
+          
         </Switch>
       </>
     </Router>
